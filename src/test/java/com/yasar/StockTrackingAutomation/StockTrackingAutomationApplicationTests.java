e2b5bf12e77f8027fb103ed6d4397f2bab6feeed
ysrbal <ysr.bal@gmail.com>
2024-09-11 11:28:29 +0300

    first commit

---
+++ src/test/java/com/yasar/StockTrackingAutomation/StockTrackingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasar.StockTrackingAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackingAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
